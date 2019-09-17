ed59d39d1bc84f76239324cf839a791341734820
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-17 15:13:06 -0500

    created links and local storage hook file

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,13 +6,12 @@ import './App.css';
 
 function App() {
   return (
-    <
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Link to='/login'>login</Link>
+        <Link to='/register'>register</Link>
+        <Form />
         </header>
     </div>
   );
--- friends/src/components/Form.js
+++ friends/src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Form = () => {
 
-    if(isLoading)
+    // if(isLoading)
     return (
         <>
         <form>
