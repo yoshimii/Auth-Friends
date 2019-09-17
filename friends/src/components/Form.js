83a6cbf1a355048c47fa04703849e1c2accb9c9b
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-17 14:38:00 -0500

    created react app with dependencies

---
+++ friends/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <>
+        <form>
+            <input
+            type='text'
+            value=''
+            placeholder='insert information'
+            >
+            </input>
+        </form>
+        </>
+    )
+}
+
